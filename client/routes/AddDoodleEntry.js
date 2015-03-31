681a5a117e7a3eec23bac05e0ff55436cba268cf
Katie Steinberg <csteinb@umich.edu>
2015-03-31 10:07:14 -0400

    drawing works but doesn't save

--- client/routes/AddDoodleEntry.js
+++ client/routes/AddDoodleEntry.js
@@ -2,19 +2,39 @@ var React = require('react');
 var Router = require('react-router');
 var ServerRequests = require('../utils/ServerRequests');
 var AddDoodleEntryCom = require('../components/AddDoodleEntry');
+var GraffitiCom = require('../components/Graffiti');
 var Authentication = require('../utils/Authentication');
 
 
 var AddDoodleEntry = React.createClass({
 	mixins: [Authentication],
 
+	getInitialState: function(){
+		return {
+			canvasFunc: null,
+			readOnly: false
+		};
+	},
+
 	render: function(){
 		return (
 			<div className="container">
-				<AddDoodleEntryCom />
+				<div className="col-xs-12 col-md-7">
+					<GraffitiCom
+						readOnly={this.state.readOnly}
+						registerCanvas={this.registerCanvas}
+					/>
+				</div>
 			</div>
 		);
+	},
+
+	registerCanvas: function(getCanvasFunc){
+		this.setState({
+			canvasFunc: getCanvasFunc
+		});
 	}
+
 });
 
 module.exports = AddDoodleEntry;
