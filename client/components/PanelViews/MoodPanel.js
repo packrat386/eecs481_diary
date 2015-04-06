7e50bc9209c42ecbb947b632011c2a1c20b0fb82
hning <hning@umich.edu>
2015-04-05 22:23:04 -0400

    Edit mood from list

--- client/components/PanelViews/MoodPanel.js
+++ client/components/PanelViews/MoodPanel.js
@@ -6,18 +6,84 @@ function getImageSource(value){
 	return "/images/pain-"+value+".png";
 }
 
+var painList = [0, 2, 4, 8, 10];
+
 var MoodPanel =  React.createClass({
 
+	getInitialState: function(){
+
+		return {
+			value: this.props.entry.get("data").value
+		};
+	},
+
+	componentDidMount: function(){
+		console.log("Mood mounted");
+		if(this.props.registerCallback){
+			this.props.registerCallback(this._getData);
+		}
+	},
+
+	_getData: function(){
+		if(this.state.value === null){
+			return null;
+		} else {
+			return {
+				value: this.state.value
+			};		
+		}
+	},
+	
+	_setMood: function(event){
+		event.preventDefault();
+		var value = event.target.value;
+		console.log(value);
+
+		this.setState({
+			value: value
+		});
+	},
+
 	render: function(){
-		return (<span>
-				<img
+
+		var moodComponents = null;
+		if(this.props.edit === true){
+			var selectedStyle = {
+				height: "250px",
+				borderStyle: "solid",
+				borderColor: "black"
+			};
+			var unselectedStyle = {
+				height: "250px",
+				borderStyle: "none"
+			};
+
+			moodComponents = painList.map(function(pain){
+				var style = pain == this.state.value ? selectedStyle : unselectedStyle;
+				console.log(this.state.value + " " + pain);
+				return (<input 
+					type="image" 
+					className=".col-md-3"
+					style={style}
+					key={pain}
+					src={getImageSource(pain)}
+					value={pain}
+					onClick={this._setMood} 
+				/>);
+			}.bind(this));
+		} else {
+			moodComponents = (<img
 					style={
 						{
 							height: "250px",
 							paddingBottom: "20px"
 						}
 					}
-					src={getImageSource(this.props.entry.get("data").value)} />
+			src={getImageSource(this.props.entry.get("data").value)} />);
+		}
+
+		return (<span>
+				{moodComponents}
 			</span>);
 	}
 
