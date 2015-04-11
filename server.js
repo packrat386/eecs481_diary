e35e7184440168c3b510e87c0b2bd64f83b8b77d
hning <hning@umich.edu>
2015-04-11 14:30:08 -0400

    Added code necessary for https

--- server.js
+++ server.js
@@ -39,7 +39,11 @@ app.use('/js', browserify('./client/scripts', {
 app.get('*', function(req, res) {
 	// this route will respond to all requests with the contents of your index
 	// template. Doing this allows react-router to render the view in the app.
+    
+    // if (req.headers["x-forwarded-proto"] === "https")
     res.render('index.html');
+    // else 
+    // 	res.redirect("https://" + req.headers.host + req.url); 
 });
 
 // start the server
